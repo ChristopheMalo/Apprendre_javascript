d4d192bf6d63b906642c381cef8b1a226cb272ab
ChristopheMalo <christof.malo@gmail.com>
2015-10-16 20:01:16 +0200

    Coder chapitre 01 exo 01 Presentation

---
+++ js/chapitre_01/01_presentation.js
@@ -0,0 +1,8 @@
+/* 
+ * Code Javascript chapitre 01 exercice 01 - Présentation
+ *
+ */
+/* Déclaration des variables */
+var myFirstname = 'Christophe', myAge = 45;
+
+console.log('Je m\'appelle ' + myFirstname + '.\n' + 'J\'ai ' + myAge + ' ans.');
