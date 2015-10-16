e8c48bcb6822f86d7a469220268556ecd34ab741
ChristopheMalo <christof.malo@gmail.com>
2015-10-16 23:23:08 +0200

    Coder chapitre 01 exo 02 mini-calculatrice

---
+++ js/chapitre_01/02_mini_calculatrice.js
@@ -0,0 +1,13 @@
+/* 
+ * Code Javascript chapitre 01 exercice 02 - Mini-calculatrice
+ *
+ */
+/* Déclaration des variables */
+var number01 = 6, number02 = 3;
+
+/* Affichage des informations dans la console */
+console.log('Nombre 1 : ' + number01 + ' -- ' + 'Nombre 2 : ' + number02);
+console.log('Résultat addition : ' + (number01 + number02)); // Addition
+console.log('Résultat soustraction : ' + (number01 - number02)); // Soustraction
+console.log('Résultat multiplication : ' + (number01 * number02)); // Multiplication
+console.log('Résultat division : ' + (number01 / number02)); // Division
