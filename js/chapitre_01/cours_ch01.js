6cd65646b5e00a2f5c635e9622a88bf0cb059b69
ChristopheMalo <christof.malo@gmail.com>
2015-10-20 10:01:23 +0200

    Coder les exemples du cours ch01

---
+++ js/chapitre_01/cours_ch01.js
@@ -0,0 +1,8 @@
+/* 
+ * Code Javascript - Cours - Chapitre 01
+ */
+console.log('Bonjour en Javascript !');
+console.log('Quelques calculs');
+console.log(4 + 7);
+console.log(12 / 0);
+console.log('Bye bye :)');
