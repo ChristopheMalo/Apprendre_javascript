26e4c7ca2912bb094e6877d241f3329497348744
ChristopheMalo <christof.malo@gmail.com>
2015-10-20 11:36:17 +0200

    Coder exercice 02 ch02 Saisie d'un nombre

---
+++ js/chapitre_02/02_saisie_nombre.js
@@ -0,0 +1,11 @@
+/* 
+ * Code Javascript chapitre 02 exercice 01 - Bonjour
+ *
+ */
+var saisieNombre = prompt('Saisis un nombre :');
+var nombre = Number(saisieNombre);
+console.log('Le nombre saisi : ' + nombre);
+
+/* Saisie et conversion en une seule ligne de code */
+var saisieNombre = Number(prompt('Saisis un nouveau nombre :'));
+console.log('Le nombre saisi : ' + saisieNombre);
