d08af619411ed75786e6f790006a3376a50d36a8
ChristopheMalo <christof.malo@gmail.com>
2015-10-20 12:13:49 +0200

    Coder exercice 03 ch02 Bonjour amélioré

---
+++ js/chapitre_02/03_bonjour_upgrade.js
@@ -0,0 +1,8 @@
+/* 
+ * Code Javascript chapitre 02 exercice 03 - Bonjour amélioré
+ *
+ */
+/* Interaction avec l'utilisateur */
+var firstname   = prompt('Saisis ton prénom :');
+var lastname    = prompt('Saisis ton nom :');
+console.log('Bonjour, ' + firstname + ' ' + lastname);
