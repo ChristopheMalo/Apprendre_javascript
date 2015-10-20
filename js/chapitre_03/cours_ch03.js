97d9b8eec52dbe0f95fef8458cca3a3757fb3304
ChristopheMalo <christof.malo@gmail.com>
2015-10-20 15:31:00 +0200

    Décommenter les exemples

--- js/chapitre_03/cours_ch03.js
+++ js/chapitre_03/cours_ch03.js
@@ -2,87 +2,87 @@
  * Code Javascript - Cours - Chapitre 03
  */
 /* Conditions */
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if (nombreSaisi > 0) {
-//    console.log(nombreSaisi + ' est un nombre positif.');
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if (nombreSaisi > 0) {
+    console.log(nombreSaisi + ' est un nombre positif.');
+}
 
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if (nombreSaisi >= 0) {
-//    console.log(nombreSaisi + ' est positif ou nul.');
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if (nombreSaisi >= 0) {
+    console.log(nombreSaisi + ' est positif ou nul.');
+}
 
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if (nombreSaisi > 0) {
-//    console.log(nombreSaisi + ' est un nombre positif.');
-//} else {
-//    console.log(nombreSaisi + ' est négatif ou nul');
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if (nombreSaisi > 0) {
+    console.log(nombreSaisi + ' est un nombre positif.');
+} else {
+    console.log(nombreSaisi + ' est négatif ou nul');
+}
 
 
 /* Methode 1 */
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if (nombreSaisi > 0) {
-//    console.log(nombreSaisi + ' est un nombre positif.');
-//} else { // nombre <= 0
-//    if (nombreSaisi < 0) { // nombre < 0
-//        console.log(nombreSaisi + ' est négatif');
-//    } else { // nombre === 0
-//        console.log(nombreSaisi + ' est nul');
-//    }
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if (nombreSaisi > 0) {
+    console.log(nombreSaisi + ' est un nombre positif.');
+} else { // nombre <= 0
+    if (nombreSaisi < 0) { // nombre < 0
+        console.log(nombreSaisi + ' est négatif');
+    } else { // nombre === 0
+        console.log(nombreSaisi + ' est nul');
+    }
+}
 
 /* Méthode 2 */
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if (nombreSaisi > 0) {
-//    
-//    console.log(nombreSaisi + ' est un nombre positif.');
-//    
-//} else if (nombreSaisi < 0) {
-//    
-//    console.log(nombreSaisi + ' est négatif');
-//    
-//} else {
-//    
-//    console.log(nombreSaisi + ' est nul');
-//    
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if (nombreSaisi > 0) {
+    
+    console.log(nombreSaisi + ' est un nombre positif.');
+    
+} else if (nombreSaisi < 0) {
+    
+    console.log(nombreSaisi + ' est négatif');
+    
+} else {
+    
+    console.log(nombreSaisi + ' est nul');
+    
+}
 
 
 /* Conditions composées */
 /* Opérateur logique ET (&&) */
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if ((nombreSaisi >= 0) && (nombreSaisi <= 100)) {
-//    console.log(nombreSaisi + ' est compris entre 0 et 100');
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if ((nombreSaisi >= 0) && (nombreSaisi <= 100)) {
+    console.log(nombreSaisi + ' est compris entre 0 et 100');
+}
 
 /* Table de vérité de l'opérateur && */
-//console.log(true && true); // Affiche true
-//console.log(true && false); // Affiche false
-//console.log(false && true); // Affiche false
-//console.log(false && false); // Affiche false
+console.log(true && true); // Affiche true
+console.log(true && false); // Affiche false
+console.log(false && true); // Affiche false
+console.log(false && false); // Affiche false
 
 /* Opérateur logique ou (||) */
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if ((nombreSaisi < 0) || (nombreSaisi > 100)) {
-//    console.log(nombreSaisi + ' n\'est pas compris entre 0 et 100');
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if ((nombreSaisi < 0) || (nombreSaisi > 100)) {
+    console.log(nombreSaisi + ' n\'est pas compris entre 0 et 100');
+}
 
 /* Table de vérité de l'opérateur || */
-//console.log(true || true); // Affiche true
-//console.log(true || false); // Affiche true
-//console.log(false || true); // Affiche true
-//console.log(false || false); // Affiche false
+console.log(true || true); // Affiche true
+console.log(true || false); // Affiche true
+console.log(false || true); // Affiche true
+console.log(false || false); // Affiche false
 
 /* Opérateur logique ou (||) */
-//var nombreSaisi = Number(prompt('Saisis un nombre :'));
-//if (!(nombreSaisi > 100)) {
-//    console.log(nombreSaisi + ' est inférieur ou égal à 100');
-//}
+var nombreSaisi = Number(prompt('Saisis un nombre :'));
+if (!(nombreSaisi > 100)) {
+    console.log(nombreSaisi + ' est inférieur ou égal à 100');
+}
 
 /* Table de vérité de l'opérateur || */
-//console.log(!true); // Affiche false
-//console.log(!false); // Affiche true
+console.log(!true); // Affiche false
+console.log(!false); // Affiche true
 
 /* CHoix */
 var meteo = prompt("Quel temps fait-il dehors ?");
