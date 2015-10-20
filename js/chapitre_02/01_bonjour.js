462ddae1f0f91a99a905bf2decbae84f65b856fb
ChristopheMalo <christof.malo@gmail.com>
2015-10-20 11:25:17 +0200

    Coder exercice 01 ch02 bonjour

---
+++ js/chapitre_02/01_bonjour.js
@@ -0,0 +1,7 @@
+/* 
+ * Code Javascript chapitre 02 exercice 01 - Bonjour
+ *
+ */
+/* Interaction avec l'utilisateur */
+var firstname = prompt('Saisis ton prénom :');
+console.log('Bonjour, ' + firstname);
