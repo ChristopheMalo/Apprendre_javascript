83ed09b7e5ceef9701e212b6c474b791d2e81f5a
ChristopheMalo <christof.malo@gmail.com>
2015-10-20 12:53:26 +0200

    Coder exercice 05 ch02 calculer la TVA

---
+++ js/chapitre_02/05_calcul_tva.js
@@ -0,0 +1,15 @@
+/* 
+ * Code Javascript chapitre 02 exercice 04 - Valeurs
+ *
+ */
+
+var prixHT = Number(prompt('Saisis un prix HT ')); //
+var prixTTC = prixHT * 1.2; // TVA à 20%
+
+console.log('Pour un prix HT de ' + prixHT + ' euros, le prix TTC est de ' + prixTTC + ' euros.');
+console.log('LA TVA est à 20% dans cet exercice.');
+
+/* Améliorations pour la la TVA en variable */
+var TVA = 20 / 100;
+var prixTTC = prixHT * (1 + TVA);
+console.log('TVA avec variable - Prix TTC : ' + prixTTC + ' euros.');
