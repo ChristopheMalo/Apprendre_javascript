a5ef107457ed3a436640b1099bf0f3a0c4d94364
ChristopheMalo <christof.malo@gmail.com>
2015-10-21 13:28:36 +0200

    Coder exercice 06 ch04 Triangle

---
+++ js/chapitre_04/06_triangle.js
@@ -0,0 +1,13 @@
+/* 
+ * Code Javascript chapitre 4 exercice 06 - Triangle
+ *
+ */
+/* Boucle la plus adéquate : For
+ * 
+ */
+var signe = '';
+
+for (var i = 0; i < 7; i++) {
+    signe += '#';
+    console.log(signe);
+}
